f1db17f53e0f201ad70ca41cd990d8355069429e
Victoria Aloy <victoria.aloy@etermax.com.uy>
2023-02-20 18:03:33 -0300

    Projects Added

---
+++ my_android/app/src/main/java/com/valoy/myandroid/FlutterStartActivity.kt
@@ -0,0 +1,56 @@
+package com.valoy.myandroid
+
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
+import android.util.Log
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class FlutterStartActivity : FlutterFragmentActivity() {
+
+    companion object {
+        fun withCachedEngine(engineId: String) = CustomCachedEngineIntentBuilder(engineId)
+    }
+
+    class CustomCachedEngineIntentBuilder(engineId: String) :
+        CachedEngineIntentBuilder(FlutterStartActivity::class.java, engineId)
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.valoy.myandroid/demo")
+            .setMethodCallHandler { call, result ->
+                Log.d("FlutterStartActivity", "call from flutter")
+                if(call.method == "startMainActivity"){
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else if (call.method == "getBatteryLevel") {
+                    val batteryLevel = getBatteryLevel()
+                    if (batteryLevel != -1) {
+                        result.success(batteryLevel)
+                    } else {
+                        result.error("UNAVAILABLE", "Battery level not available.", null)
+                    }
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+
+}
